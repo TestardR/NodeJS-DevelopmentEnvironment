232c4ca6f0ff07bef21395bfaf8518845ca26f3f
Romain <rmtestard@gmail.com>
2018-12-05 19:51:52 +0100

    Production Webpack Configuration with Minification

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import WebpackMd5Hash from 'webpack-md5-hash';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import WebpackMd5Hash from 'webpack-md5-hash';
+// import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -20,10 +20,10 @@ export default {
   },
   plugins: [
     // Generate an external css file with a hash in the filename
-    new ExtractTextPlugin('[name].[contenthash].css'),
+    // new ExtractTextPlugin('[name].[contenthash].css'),
 
     // Hash the files using MD5 so that their names change when the content changes.
-    new WebpackMd5Hash(),
+    // new WebpackMd5Hash(),
 
     // Use CommonsChunkPlugin to create a separate bundle
     // of vendor libraries so that they're cached separately.
@@ -32,25 +32,25 @@ export default {
     }),
 
     // Create HTML file that includes reference to bundled JS.
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true
-      },
-      inject: true,
-      // Properties you define here are available in index.html
-      // using htmlWebpackPlugin.options.varName
-      trackJSToken: '43ad216f57d94259968435894490a5c7'
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: 'src/index.html',
+    //   minify: {
+    //     removeComments: true,
+    //     collapseWhitespace: true,
+    //     removeRedundantAttributes: true,
+    //     useShortDoctype: true,
+    //     removeEmptyAttributes: true,
+    //     removeStyleLinkTypeAttributes: true,
+    //     keepClosingSlash: true,
+    //     minifyJS: true,
+    //     minifyCSS: true,
+    //     minifyURLs: true
+    //   },
+    //   inject: true,
+    //   // Properties you define here are available in index.html
+    //   // using htmlWebpackPlugin.options.varName
+    //   trackJSToken: '43ad216f57d94259968435894490a5c7'
+    // }),
 
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
@@ -60,8 +60,8 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] }
+      // { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
     ]
   }
 };
