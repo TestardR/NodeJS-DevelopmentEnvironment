4709f53c2b7f8cd2a0321f186b2a41ec79c7388f
Romain <rmtestard@gmail.com>
2018-12-06 20:10:42 +0100

    cache busting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-// import WebpackMd5Hash from 'webpack-md5-hash';
+import WebpackMd5Hash from 'webpack-md5-hash';
 // import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
@@ -23,13 +23,13 @@ export default {
     // new ExtractTextPlugin('[name].[contenthash].css'),
 
     // Hash the files using MD5 so that their names change when the content changes.
-    // new WebpackMd5Hash(),
+    new WebpackMd5Hash(),
 
     // Use CommonsChunkPlugin to create a separate bundle
     // of vendor libraries so that they're cached separately.
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor'
-    // }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
 
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
